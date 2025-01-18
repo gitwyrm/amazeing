86219e9c021b0b2aa5139d98ec405388fda7651b
gitwyrm <121982991+gitwyrm@users.noreply.github.com>
2025-01-18 23:43:41 +0100

    Add Line and Point classes; implement drawing functionality in Window class

---
+++ src/line.py
@@ -0,0 +1,11 @@
+from tkinter import Canvas
+from point import Point
+
+
+class Line():
+    def __init__(self, start: Point, end: Point):
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas = Canvas, color="black"):
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=color)
--- src/main.py
+++ src/main.py
@@ -1,29 +1,13 @@
-from tkinter import Tk, BOTH, Canvas
+from line import Line
+from point import Point
+from window import Window
+
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(10, 10), Point(100, 100)))
+    win.draw_line(Line(Point(10, 100), Point(100, 10)), "red")
     win.wait_for_close()
-
-class Window():
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("aMAZEing")
-        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
-        self.__canvas.pack(fill=BOTH, expand=1)
-        self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        self.__running = True
-        while self.__running:
-            self.redraw()
-
-    def close(self):
-        self.__running = False
 
 if __name__ == "__main__":
     main()
