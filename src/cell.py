f8cd5497940d1ff850c0fd8b46074700ac885e12
gitwyrm <121982991+gitwyrm@users.noreply.github.com>
2025-01-19 00:08:02 +0100

    Enhance Cell class with draw_move method for visualizing moves between cells

--- src/cell.py
+++ src/cell.py
@@ -1,9 +1,10 @@
 from line import Line
 from point import Point
+from window import Window
 
 
 class Cell():
-    def __init__(self, x1, y1, x2, y2, window):
+    def __init__(self, x1, y1, x2, y2, window: Window):
         self.has_left_wall = True
         self.has_top_wall = True
         self.has_right_wall = True
@@ -23,3 +24,13 @@ class Cell():
             self.__win.draw_line(Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)))
         if self.has_bottom_wall:
             self.__win.draw_line(Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)))
+
+    def draw_move(self, to_cell, undo=False):
+        """
+        Draw a move between two cells, a line should go from the center of one cell to the center of the other
+        
+        If undo is True, the line is drawn gray, otherwise red
+        """
+        color = "gray" if undo else "red"
+        self.__win.draw_line(Line(Point(self.__x1 + 50, self.__y1 + 50), Point(to_cell.__x1 + 50, to_cell.__y1 + 50),), color)
+        
--- src/main.py
+++ src/main.py
@@ -11,8 +11,11 @@ def main():
         Cell(100, 0, 200, 100, win),
         Cell(200, 0, 300, 100, win),
         Cell(300, 0, 400, 100, win),
+        Cell(100, 100, 200, 200, win),
     ]
     cells[2].has_bottom_wall = False
+    cells[2].draw_move(cells[3], undo=True)
+    cells[1].draw_move(cells[4], undo=False)
 
     for cell in cells:
         cell.draw()
